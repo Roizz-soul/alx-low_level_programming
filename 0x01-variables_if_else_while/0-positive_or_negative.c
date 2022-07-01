dcfadec01c2ce61d5def5181e301e7f4e56676f1
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-01 12:38:29 -0700

    If and else conditions on random numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this code shows conditional statements
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	/*if and else statements to establish conditions*/
+	if (n > 0)
+	{printf("%d is positive\n", n);
+	} else if (n < 0)
+	{printf("%d is negative\n", n);
+	} else {
+		printf("%d is zero\n", n);
+	}
+	
+	return (0);
+
+}
