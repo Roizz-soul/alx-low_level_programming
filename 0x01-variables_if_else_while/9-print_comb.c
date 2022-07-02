b4a602fb7efa4ebefda2e3be9b71acbd1aaafa2b
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-01 17:41:59 -0700

    Exploring putchar and continue

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Applications of putchar and continue
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++){
+		putchar((c % 10) + '0');
+		if (c == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
