eab1abe54893a5ff39054dee08d794d05738b1f3
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-05 14:41:16 -0700

    printing the alphabet 10 times on a line each

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times on each line
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+
+		_putchar('\n');
+	}
+}
