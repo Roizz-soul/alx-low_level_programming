63aad6fadaeba7bda4cb6e5f0a759a8c032504f5
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-05 16:13:19 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign if a number
+ * @n: character to ve checked
+ *
+ * Return: check code
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	return (0);
+}
