d71f249ea26b4cae88ad907415392bbc24d8d60c
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-07 15:32:06 -0700

    printing set of numbers on many lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers 0 to 14 tentimes on each line
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
