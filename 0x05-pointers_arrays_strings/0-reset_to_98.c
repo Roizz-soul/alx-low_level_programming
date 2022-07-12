d94e6c38a8eb78fed4e03f08ff9f509bf5ce2f4f
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-12 09:28:58 -0700

    Changing to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Resets any integer in the bracket to 98
+* @n: pointer to the integer to be put in
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# Pointers, Arrays and Strings
+
+learning about pointers, addresses, dereferencing, data structuring using arrays and strings,..., beautiful stuffs.
