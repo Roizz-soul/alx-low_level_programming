a8e76535132ac29b59c382508560a5dd4a74e915
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-18 09:34:59 -0700

    Y'

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,21 +9,27 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i;
 
 	if (*needle == 0)
 		return (haystack);
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	while (*haystack)
 	{
-		if (haystack[i] == needle[0])
-		{
-			for (j = 0; needle[j] != '\0'; j++)
-				;
+		i = 0;
 
-			if (needle[j--] == haystack[i + j--])
-				return (haystack + i);
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				i++;
+
+			} while (haystack[i] == needle[i]);
 		}
+
+		haystack++;
 	}
 
 	return (0);
