5d20557726edfe3e94c3a85aadfbcc2f6590ade7
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-18 12:34:33 -0700

    Diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,20 +10,20 @@
 
 void print_diagsums(int *a, int size);
 {
-	int i, s1 = 0, s2 = 0;
+	int i, j, s1 = 0, s2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		s1 += a[i];
-		a += size;
+		j + 0;
+		s1 += a[i][j];
+		j += 1;
 	}
 
-	a -= size;
-
 	for (i = 0; i < size; i++)
 	{
-		s2 += a[i];
-		a -= size;
+		j = size - 1;
+		s2 += a[i][j];
+		j -= 1;
 	}
 
 	printf("%d, %d\n", s1, s2);
