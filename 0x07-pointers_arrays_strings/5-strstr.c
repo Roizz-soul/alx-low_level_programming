0c392f6ea89f5a7e634aebaf9315854ccf2e0561
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-18 08:38:43 -0700

    Yay

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,6 +11,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
+	if (*needle == 0)
+		return (haystack);
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
