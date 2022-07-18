f864d1cdb3ca09f1aa7d7da1eef9213a1fbc4c3c
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-18 13:04:05 -0700

    s

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,23 +7,23 @@
   * @size: size of the array
   */
 
-void print_diagsums(int *a, int size);
+void print_diagsums(int *a, int size)
 {
 	int i, j, s1 = 0, s2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		j + 0;
-		s1 += a[j];
+		s1 += a[i][j];
 		j += 1;
 	}
 
 	for (i = 0; i < size; i++)
 	{
 		j = size - 1;
-		s2 += a[j];
+		s2 += a[i][j];
 		j -= 1;
 	}
 
-	printf("%d, %d\n", s1, s2);
+	printf("%i, %i\n", s1, s2);
 }
