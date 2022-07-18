55e4a2a4efca41474aaab829a37687301d2aaf5f
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-18 08:40:51 -0700

    Yelp

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,7 +19,7 @@ char *_strstr(char *haystack, char *needle)
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i++] == needle[j++])
-				return (s + i);
+				return (haystack + i);
 		}
 	}
 
