292342a6299489576494e799585808f437ffa47d
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-20 09:51:26 -0700

    Counting the string lenth

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,15 +8,13 @@
 
 int _strlen_recursion(char *s)
 {
+	int i = 0;
+
 	if (*s)
 	{
-		int i;
-
-		if (*S == '\0')
-			i = 0;
-		else
-			i = 1;
-
-		return (i + _strlen_recursion(s + 1));
+		i++;
+		i += _strlen_recursion(s + 1);
 	}
+
+	return (i);
 }
