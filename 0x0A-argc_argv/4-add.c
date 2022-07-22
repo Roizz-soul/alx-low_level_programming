bdb10cddbf45a73c77bf35304ec1e273164f5003
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-22 10:37:55 -0700

    Adding only integers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: Always 0.
+  */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = o;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
