7831b4c0e0396274ac55285a8f2a93f3fbcfb7e1
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-26 08:45:10 -0700

    e

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,25 +20,26 @@ char *str_concat(char *s1, char *s2)
 	if (i > 0)
 		i = i + 1;
 	else if (s1 == NULL)
+	{
+		s1 = "";
 		i = 0;
+	}
 	if (j > 0)
 		j = j + 1;
 	else if (s2 == NULL)
+	{
+		s2 = "";
 		j = 0;
+	}
 	k = i + j - 1;
 
 	strsum = malloc(sizeof(char) * k);
 
 	if (strsum == NULL)
 		return (NULL);
-	if (s1 == NULL)
-		s1 = "";
 
 	for (l = 0; s1[l] != '\0'; l++)
 		strsum[l] = s1[l];
-
-	if (s2 == NULL)
-		s2 = "";
 
 	for (m = 0; s2[m] != '\0'; m++)
 		strsum[l++] = s2[m];
