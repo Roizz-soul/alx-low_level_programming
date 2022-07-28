629652853958e1b495090db0706c75d84a69f7fe
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-28 09:35:40 -0700

    mul 2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,6 +14,17 @@ int main(int argc, char **argv)
 		printf("%s\n", "Error");
 		exit(98);
 	}
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || arg[i][j] > '9')
+			{
+				printf("%s\n", "Error");
+				exit(98);
+			}
+		}
+	}
 	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
