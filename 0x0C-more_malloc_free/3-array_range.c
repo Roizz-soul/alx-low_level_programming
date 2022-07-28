3f7d1668390b075e7f8f5c97100d74a8ba042e50
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-28 08:04:25 -0700

    Range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * array_range - creates an array of integers
+  * @min: minimum value of element in the array
+  * @max: maximum value
+  * Return: check code
+  */
+int *array_range(int min, int max)
+{
+	int *array;
+	int i,j;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * (max - min + 1));
+
+	if (array == 0)
+		return (NULL);
+
+	j = 0;
+
+	for (i = min, i <= max, i++)
+		array[j++] = min++;
+
+	return (array);
+}
