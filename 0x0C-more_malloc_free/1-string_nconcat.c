4b9d684a7127cd38a154f9e958061ec1b3efacf0
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-28 05:44:52 -0700

    Concatenating strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * string_nconcat - concatenates two strings
+  * @s1: First string to be concatenated
+  * @s2: second string to be concatenated
+  * @n: number of elements in s2 to be added
+  * Return: check code
+  */
+
+char *string_nconcat(char *s1, char s2, unsigned int n)
+{
+	char *sum;
+	int i, j, k;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	sum = malloc(sizeof(s1) + (sizeof(char) * n));
+
+	for (i = 0; s1[i]; i++)
+		sum[k++] = s1[i];
+	for (j = 0; s2[j] && j < n; j++)
+		sum[k++] = s2[j];
+	sum[k] = '\0';
+
+	return (sum);
+}
