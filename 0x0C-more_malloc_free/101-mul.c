c44bf1a20a06d0662babcd78f1e1c5716b1ae07b
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-07-28 09:47:22 -0700

    g

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,10 +7,9 @@
   * @argv: argument vector
   * Return: check code
   */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int i, j;
-	char *fil;
 
 	if (argc != 3)
 	{
@@ -18,13 +17,11 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	fil = argv;
-
 	for (i = 1; i < 3; i++)
 	{
-		for (j = 0; fil[i][j]; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (fil[i][j] < '0' || fil[i][j] > '9')
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("%s\n", "Error");
 				exit(98);
