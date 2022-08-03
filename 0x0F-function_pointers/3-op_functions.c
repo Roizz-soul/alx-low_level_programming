5e45788b99c0776d8b55f31112bf48d0b45b513b
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-08-03 09:53:58 -0700

    Calculation

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+  * op_add - addition operation
+  * @a: First number
+  * @b: Second number
+  * Return: the solution which is an integer
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - addition operation
+  * @a: First number
+  * @b: Second number
+  * Return: the solution which is an integer
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - addition operation
+  * @a: First number
+  * @b: Second number
+  * Return: the solution which is an integer
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - addition operation
+  * @a: First number
+  * @b: Second number
+  * Return: the solution which is an integer
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - addition operation
+  * @a: First number
+  * @b: Second number
+  * Return: the solution which is an integer
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
