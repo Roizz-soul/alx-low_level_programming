71a4ecdb5792102faa8654f49d2e273c5f6afba8
Oluremi Abraham <oluremiabraham9@gmail.com>
2022-08-16 01:56:39 -0700

    adj 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,13 +9,11 @@ int sum_listint(listint_t *head)
 	listint_t *temp = head;
 	int sum = 0;
 
-	if (temp == NULL)
-		return (0);
-	while (temp->next != NULL)
+	while (temp)
 	{
 		sum += temp->n;
 		temp = temp->next;
 	}
-	sum += temp->next;
+
 	return (sum);
 }
