40a92b5312319c788248b5aa7556b79b3247001d
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-09-30 02:56:21 +0000

    Trimmings

--- 0x17-doubly_linked_lists/try.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def my_function(x):
-	  return x[::-1]
-alist = []
-for i in range(100, 1000):
-	for j in range(100, 1000):
-		if str(i * j) == my_function(str(i * j)):
-			alist.append(i * j)
-print(max(alist))
