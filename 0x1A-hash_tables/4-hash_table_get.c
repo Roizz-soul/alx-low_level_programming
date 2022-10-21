7466ca6f91dc6c6a15acec526eb00efc787a7330
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-21 13:58:52 +0000

    fh

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp;
 
-	if (key == null || *key == '\n')
+	if (key == NULL || *key == '\n')
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
