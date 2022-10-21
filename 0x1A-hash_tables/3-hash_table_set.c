3e3c2ba462817f5b7082dd8d0bf4fffc344f55b3
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-21 13:11:56 +0000

    gj

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	unsigned long int index;
+	unsigned long int index, i;
 	char *val, *k;
 
 	new = malloc(sizeof(hash_node_t));
@@ -19,14 +19,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht && k && val && new && key && (value != NULL))
 	{
 		index = key_index((const unsigned char *)key, ht->size);
+		for (i = index; ht->array[i]; i++)
+		{
+			if (strcmp(ht->array[i]->key, k) == 0)
+			{
+				free(ht->array[i]->value);
+				ht->array[i]->value = val;
+				return (1);
+			}
+		}
 		new->key = k;
 		new->value = val;
 		new->next = ht->array[index];
 		ht->array[index] = new;
 		return (1);
 	}
-	free(new);
-	free(val);
-	free(k);
 	return (0);
 }
