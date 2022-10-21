ecb09327004ae9b1136fa6137b1649837f398280
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-21 13:49:00 +0000

    4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+  * hash_table_get - retrieves a value associated with a key
+  * @ht: hash table
+  * @key: key we're looking for
+  * Return: value or NULL if not found
+  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL)
+}
