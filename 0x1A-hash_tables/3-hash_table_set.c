c364411e0ec29f6687f06f3cfbd048f8f6b5faca
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-10-21 13:02:37 +0000

    th

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,49 +8,22 @@
   */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char *val, *k;
+	hash_node_t *new;
 	unsigned long int index;
-	hash_node_t *temp, *new;
+	char *val, *k;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
+	new = malloc(sizeof(hash_node_t));
+	val = strdup(value);
+	k = strdup(key);
 
-	index = hash_djb2((const unsigned char *)key) % ht->size;
-	if (ht->array[index] == NULL)
+	if (ht && k && val && new && key && (value != NULL))
 	{
-		new = malloc(sizeof(hash_node_t));
-		if (new == NULL)
-			return (0);
-		val = strdup(value);
-		if (val == NULL)
-			return (0);
-		k = strdup(key);
-		if (k == NULL)
-			return (0);
+		index = key_index((const unsigned char *)key, ht->size);
 		new->key = k;
 		new->value = val;
-		new->next = NULL;
-
+		new->next = ht->array[index];
 		ht->array[index] = new;
-
 		return (1);
 	}
-
-	temp = ht->array[index];
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (0);
-	val = strdup(value);
-	if (val == NULL)
-		return (0);
-	k = strdup(key);
-	if (k == NULL)
-		return (0);
-	new->key = k;
-	new->value = val;
-	new->next = temp;
-
-	ht->array[index] = new;
-
-	return (1);
+	return (0);
 }
